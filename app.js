2b143dfe92b3179734ded9218e22b5fcb3daacfa
hasanuzzaman-dev <hasanuzzaman.dev@gmail.com>
2022-11-29 16:21:20 +0600

    set error handler

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+require("dotenv").config();
+
+const app = express();
+
+app.get('/', async (req, res, next) => {
+    res.send("Hello from express");
+})
+
+// error handling
+app.use(async (req, res, next) => {
+    // const error = new Error('Not Found!');
+    // error.status = 404;
+    // next(error);
+
+    //next(createError.NotFound());
+    
+    next(createError.NotFound('This route does not exist!'))
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        }
+    })
+})
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running at PORT: ${PORT}`);
+})
